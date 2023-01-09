7e22896cfb40d16758ff64df25f8f0de6faab42c
suvam <suvampravu@gmail.com>
2023-01-10 01:06:13 +0530

    This is my 2nd commit in Day-2

--- Q7_lab1.java
+++ Q7_lab1.java
@@ -1,2 +1,12 @@
 public class Q7_lab1 {
+    public static void main(String[] args) {
+        int a= 10;
+        int b=20;
+        System.out.println("Before swapping value a = "+ a +" b = "+ b);
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping value a = "+ a +" b = "+ b);
+    }
 }
---
+++ Q8_lab1.java
@@ -0,0 +1,12 @@
+public class Q8_lab1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        a=b+a;
+        b=a-b;
+        a=a-b;
+        System.out.println("Before swapping value a = "+ a +" b = "+b);
+
+    }
+}
