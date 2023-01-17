160dc5243662a735a3bbbab30f707cae04f7466f
suvam <suvampravu@gmail.com>
2023-01-17 23:16:26 +0530

    commit the changes

--- Lab Assignment 3/Q9_lab3.java
+++ Lab Assignment 3/Q9_lab3.java
@@ -1,14 +1,41 @@
 import java.util.Scanner;
 public class Q9_lab3 {
     public static void main(String[] args) {
-        //Q-Write a program to input 3 integer number a, b, c. Find the largest number among 3.
-        //Also find the 2nd largest number among 3.
+        Scanner scanner = new Scanner(System.in);
 
-        // Here is the sample output:
-        //Enter the value of a, b, c:10 30 50
-        //Largest number: 50
-        //2nd largest number: 30
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
 
-        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println("Largest number: " + a);
+            if (b > c) {
+                System.out.println(" 2nd Largest number: " + b);
+            }else {
+                System.out.println(" 2nd Largest number: " + c);
+            }
+        } else if (b > a && b > c) {
+            System.out.println("Largest number: " + b);
+            if (a > c) {
+                System.out.println(" 2nd Largest number: " + a);
+            }else {
+                System.out.println(" 2nd Largest number: " + c);
+            }
+        } else if (c > a && c > b) {
+            System.out.println("Largest number: " + c);
+            if (b > a) {
+                System.out.println(" 2nd Largest number: " + b);
+            }else {
+                System.out.println(" 2nd Largest number: " + a);
+            }
+        }
+
+
+
     }
 }
