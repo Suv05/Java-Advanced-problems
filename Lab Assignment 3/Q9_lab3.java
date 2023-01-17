098e9ba87e758bbcc447202416a1b2d238e19d3c
suvam <suvampravu@gmail.com>
2023-01-17 20:26:53 +0530

    1st commit of the day

--- Lab Assignment 3/Q9_lab3.java
+++ Lab Assignment 3/Q9_lab3.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
 public class Q9_lab3 {
     public static void main(String[] args) {
         //Q-Write a program to input 3 integer number a, b, c. Find the largest number among 3.
         //Also find the 2nd largest number among 3.
-        
+
         // Here is the sample output:
         //Enter the value of a, b, c:10 30 50
         //Largest number: 50
         //2nd largest number: 30
+
+        Scanner sc = new Scanner(System.in);
     }
 }
