eedf42265c867234040e9b92c928ae75df4e5026
suvam <suvampravu@gmail.com>
2023-01-19 00:32:57 +0530

    commit the changes of question 2 home

---
+++ Lab Assignment 3/Q2home_lab3.java
@@ -0,0 +1,12 @@
+public class Q2home_lab3 {
+    public static void main(String[] args) {
+//Q-Write a java program that prompts the user to enter an integer for today’s day of the
+//week (Sunday is 0, Monday is 1... and Saturday is 6). Also prompt the user to enter
+//the number of days after today for a future day and display the future day of the week.
+
+//Here is a sample run:
+//Enter today's day: 1
+//Enter the number of days elapsed since today: 3
+//Today is Monday and the future day is Thursday
+    }
+}
