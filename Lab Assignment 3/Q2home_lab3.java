8bb0bdb3ee872f8b8d370546221a7f1d4c2d9e1a
suvam <suvampravu@gmail.com>
2023-01-19 23:54:24 +0530

    commit the changes of Difficult question 2 home😻and it was difficult but i solve it.so now assignment 3 is finished.

--- Lab Assignment 3/Q2home_lab3.java
+++ Lab Assignment 3/Q2home_lab3.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Q2home_lab3 {
     public static void main(String[] args) {
 //Q-Write a java program that prompts the user to enter an integer for today’s day of the
@@ -8,15 +9,37 @@ public class Q2home_lab3 {
 //Here is a sample run:
 //Enter today's day: 1
 //Enter the number of days elapsed since today: 3
-//Today is Monday and the future day is Thursday
+//And today  is Monday and the future day is Thursday
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter today's day: ");
         int day = sc.nextInt();
         System.out.println("Enter the number of days elapsed since today: ");
         int day2 = sc.nextInt();
-        switch (day) {
-            case 1 -> System.out.println(" Today is Monday");
-            case 2 -> System.out.println(" Today is sunday");
+        if(day==1){
+            System.out.print("It is monday");
+        } else if (day == 2) {
+            System.out.print("It is Tuesday");
+        } else if (day == 3) {
+            System.out.print("It is Wednesday");
+        } else if (day == 4) {
+            System.out.print("It is Thursday");
+        } else if (day == 5) {
+            System.out.print("It is Friday");
+        } else if (day == 6) {
+            System.out.print("It is Saturday");
+        } else if (day == 7) {
+            System.out.print("It is Sunday");
         }
+        switch (day2 % 7) {
+            case 1 -> System.out.print(" And the future day  is Monday");
+            case 2 -> System.out.print(" And the future day  is Tuesday");
+            case 3 -> System.out.print(" And the future day  is Wednesday");
+            case 4 -> System.out.print(" And the future day  is Thursday");
+            case 5 -> System.out.print(" And the future day  is Friday");
+            case 6 -> System.out.print(" And the future day  is Saturday");
+            case 7 -> System.out.print(" And the future day  is sunday");
+
+        }
+
     }
 }
