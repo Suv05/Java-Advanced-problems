6045b7bfe621353b82ae19379cd1c4c8a38f8191
suvam <suvampravu@gmail.com>
2023-01-21 00:29:44 +0530

    Lab 4  questions 7 committing

--- Lab Assignment 4/Q7_lab4.java
+++ Lab Assignment 4/Q7_lab4.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class Q7_lab4 {
     public static void main(String[] args) {
        //Q- Write a java program to print the multiplication table of a number entered by the user.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int n=sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d * %d=%d\n ",n,i,n*i);
+        }
     }
 }
