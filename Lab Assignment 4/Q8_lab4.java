8333886044273b6f7d4e207e00b5d87cb2989a8e
suvam <suvampravu@gmail.com>
2023-01-22 23:42:20 +0530

    Lab 4  questions 8 committing

--- Lab Assignment 4/Q8_lab4.java
+++ Lab Assignment 4/Q8_lab4.java
@@ -1,6 +1,21 @@
 import java.util.*;
 public class Q8_lab4 {
     public static void main(String[] args) {
+        //Q-Write a program that generates a random integer number between 1 and 10 and asks the user to
+        //guess what the number is. If the user's guess is higher than the random number, the program
+        //should display "Too high, try again." If the user's guess is lower than the random number, the
+        //program should display "Too low, try again." The program should use a loop that repeats until
+        //the user correctly guesses the random number and display good guess.
+        // Sample run:
+        //Computer guess: 7
+        //User guess: 5
+        //Too low, try again
+        //Computer guess: 5
+        //User guess: 7
+        //Too high, try again
+        //Computer guess: 5
+        //User guess: 5
+        //Good guess
         Scanner sc = new Scanner(System.in);
         System.out.println("");
 
