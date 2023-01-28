8bd139d467ebf21c0955a1969452be278fac75a0
suvam <suvampravu@gmail.com>
2023-01-29 01:26:10 +0530

    Lab 4  questions  in fast committing

--- Lab Assignment 4/Q10_lab4.java
+++ Lab Assignment 4/Q10_lab4.java
@@ -1,5 +1,10 @@
 public class Q10_lab4 {
     public static void main(String[] args) {
+        //Q- Write a java program to print largest power of three less than or equal to N.
+
+        //Sample run:
+        //Enter a number: 100
+        //The largest power of 3 less than or equal to 100 is 81
 
     }
 }
--- Lab Assignment 4/Q9_lab4.java
+++ Lab Assignment 4/Q9_lab4.java
@@ -1,5 +1,12 @@
 public class Q9_lab4 {
     public static void main(String[] args) {
+        //Q- Write a java program to take an integer input from the user and print the input by removing all
+        //zeros.
+
+        //Example: Input = 10200 then Output = 12.
+        //Sample run:
+        //Enter an integer number: 10203040
+        //After removing 0 from number 10203040, the new number is 1234
 
     }
 }
