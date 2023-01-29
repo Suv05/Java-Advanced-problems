297028b990c4bbfade66c88935c50224cc29da49
suvam <suvampravu@gmail.com>
2023-01-30 01:33:57 +0530

    Lab 4 add questions committing

---
+++ Lab Assignment 4/Q4home_lab4.java
@@ -0,0 +1,5 @@
+public class Q4home_lab4 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Lab Assignment 4/Q5home_lab4.java
@@ -0,0 +1,5 @@
+public class Q5home_lab4 {
+    public static void main(String[] args) {
+
+    }
+}
