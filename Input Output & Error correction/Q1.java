ad606f5b9580b382cf5d3341b2dc301770d10edc
suvam <suvampravu@gmail.com>
2023-01-31 01:51:21 +0530

    Committing start from input-output question 2 set

--- Input Output & Error correction/Q1.java
+++ Input Output & Error correction/Q1.java
@@ -4,7 +4,9 @@ public class Q1 {
         // System.out.println(System.out.println("hi"));
 
         //Q2-
-
+        //System.out.print(013 + 1.94 + "C" + "S");
+        //Here 013 is the octal of Decimal 11.Decimal-> Octal Integer.toOctalString(int num)
+        //Also see the octal to decimal conversion ❤️🫡
             }
         }
 
