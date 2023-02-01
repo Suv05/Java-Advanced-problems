87924fefed38b97b1dd947d7d84d6c4fbe057d0a
suvam <suvampravu@gmail.com>
2023-02-02 02:33:41 +0530

    Committing start from input-output question 10 set

--- Input Output & Error correction/Part10_15.java
+++ Input Output & Error correction/Part10_15.java
@@ -1,5 +1,12 @@
 public class Part10_15 {
     public static void main(String[] args) {
+//Q-10
+//        int a = 2;
+//        int b = 3;
+//        int result = a && b ;
+//        System.out.println(result); //It will show error bcz & can't be applicable for int,double.float
+
+
 
     }
 }
