46b8a8117f4703520c3be49d14d3eb41fdbcc88a
suvam <suvampravu@gmail.com>
2023-02-02 02:36:54 +0530

    Committing start from input-output question 11 set

--- Input Output & Error correction/Part10_15.java
+++ Input Output & Error correction/Part10_15.java
@@ -5,8 +5,12 @@ public class Part10_15 {
 //        int b = 3;
 //        int result = a && b ;
 //        System.out.println(result); //It will show error bcz & can't be applicable for int,double.float
+//     but & is applicable to the integer.....
 
 
+        //Q11-
+        int x=-5;
+        System.out.println(~x);
 
     }
 }
