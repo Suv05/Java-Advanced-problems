1b82ba6f220a906d0ceb20f341c9ce1ac10ecfd9
suvam <suvampravu@gmail.com>
2023-02-03 23:22:03 +0530

    Committing start from input-output question 11 set

--- Input Output & Error correction/Part5_9.java
+++ Input Output & Error correction/Part5_9.java
@@ -6,15 +6,15 @@ public class Part5_9 {
 //        System.out.println(2+3+"bc"+'c'+'a'); //5bcca....1st str then char
 //        System.out.println('c'+'a'+2+3+"bc"); //201bc...1st char then str
 //        System.out.println("bc"+'c'+'a'+2+3); //bcca23...1st str then char
-//        System.out.println("bc"+('c'+'a')+(2)+3); //bc19623...char within ()...
+//        System.out.println("bc"+('c'+'a')+(2)+3); //bc19623...char within ()..
 
         //Q-6
-            int x = - 5;
-            System.out.println(x>>2);
-            int y = 4;                //It always sifts the digit that much but not 0
-            System.out.println(y>>1);//but +ve can sift till 0
+        int x = -5;
+        System.out.println(x >> 1);
+        int y = 120;
+        System.out.println(y >> 1);//but +ve can sift till 0
 
-         //Q-7
+        //Q-7
         System.out.println(10 + 15 + "Hello");
         System.out.println("Hello" + 10 + 15);
 
