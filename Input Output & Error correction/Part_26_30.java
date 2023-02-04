2e814bc2ce311ea5808d54aacb7863c0f9e4cd3d
suvam <suvampravu@gmail.com>
2023-02-05 02:38:59 +0530

    Committing start from input-output question 26_30 set

---
+++ Input Output & Error correction/Part_26_30.java
@@ -0,0 +1,50 @@
+public class Part_26_30 {
+    public static void main(String[] args) {
+        //Q-26
+//        int i = 132;
+//        short s = 15;
+//        byte b = (byte) i;
+//        int x = b + s;
+//        System.out.println("Value of x is " + x);
+
+
+        //Q-27
+//        long l = 30;
+//        int i = 50;
+//        short s = 60;
+//        byte b = 70;
+//        byte sum = (byte)(l + i + s + b);
+//        System.out.println("Sum = " + sum);
+
+
+        //Q-28
+//        byte y=5,z=-y;
+//        System.out.println(~y);
+//        System.out.println(~z);
+//        y&= ~y;
+//        System.out.println(y);
+//        byte x = -1;
+//        System.out.println(x>>>6);
+//        byte a=-5,b=-6;
+//        System.out.println(a|b);
+
+
+        //Q-29
+//        System.out.println(2!=3 && (7>8 || 6>5 ));
+//        System.out.println(!(2!=3) && (7>8 || 6>5 ));
+//        System.out.println(3==3 && z>=10 ));
+//        System.out.println(2!=3 && (7>8 || 6>5 ));
+
+
+
+        //Q-30
+//        int v=10;
+//        System.out.println(v%=3*4);int x=11;
+//        System.out.println(-x- -); System.out.println(x);
+//        x = -x- -;
+//        System.out.println(x); int y = -x- -;
+//        System.out.println(x+""+y);
+
+
+    }
+}
