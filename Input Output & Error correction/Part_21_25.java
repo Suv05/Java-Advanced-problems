2f1bc0c112a7c2beacb1f75b00ff19700a910061
suvam <suvampravu@gmail.com>
2023-02-05 02:33:35 +0530

    Committing start from input-output question 21_25 set

---
+++ Input Output & Error correction/Part_21_25.java
@@ -0,0 +1,57 @@
+public class Part_21_25 {
+    public static void main(String[] args) {
+        //Q-21
+//        int a=10;
+//        int b=5;
+//        int c=20;
+//        System.out.println(a < b&&a++ < c);
+//        System.out.println(a);
+//        System.out.println(a < b&a++ < c);
+//        System.out.println(a);
+
+        //Q-22
+//        int a=10;
+//        int b=6;
+//        int c=30;
+//        System.out.println(a > b || a < c);
+//        System.out.println(a > b | a < c);
+//        System.out.println(a > b || a++ < c);
+//        System.out.println(a);
+//        System.out.println(a > b | a ++ < c);
+//        System.out.println(a);
+
+        //Q-23
+//        int a=4;
+//        int b=5;
+//        int x=(a++ < b)?a:b;//5 : 5
+//        int y=a+b-x;
+//        System.out.println("x="+x);
+//        System.out.println("y="+y);
+
+
+        //Q-24
+//        int a=10;
+//        a+=3;
+//        System.out.println(a);
+//        a-=4;
+//        System.out.println(a);
+//        a*=2;
+//        System.out.println(a);
+//        a/=2;
+//        System.out.println(a);
+
+
+        //Q-25
+//        long l = 55;
+//        int i = 44;
+//        short s = 33;
+//        byte b = 22;
+//        i = (int) l;
+//        s = (short) i;
+//        b = (byte) s;
+//        System.out.println("l = " + l);
+//        System.out.println("i = " + i);
+//        System.out.println("s = " + s);
+//        System.out.println("b = " + b);
+    }
+}
