c058f3b07c20c05a4a848fbc14756ea5dfcb6779
suvam <suvampravu@gmail.com>
2023-02-06 01:52:04 +0530

    Lab 4 add committing answer

--- Lab Assignment 4/Q4home_lab4.java
+++ Lab Assignment 4/Q4home_lab4.java
@@ -1,5 +1,10 @@
 public class Q4home_lab4 {
     public static void main(String[] args) {
+        //Q-Write a java program to print the sum of all even numbers and the product of all odd numbers
+        //from 1 to N. Where, N is the input to the program.
+        //For input, N = 10
+        //Sum of all even numbers = 2 + 4 + 6 + 8 + 10 = 30
+        //Product of all odd numbers = 1 * 3 * 5 * 7 * 9 = 945
 
     }
 }
--- Lab Assignment 4/Q5home_lab4.java
+++ Lab Assignment 4/Q5home_lab4.java
@@ -1,5 +1,12 @@
 public class Q5home_lab4 {
     public static void main(String[] args) {
+        //Q-Write a java program to print the following output using loop. Where, input is the number of
+        //rows in output pattern.
+        //For input, N = 4.
+        //1
+        //121
+        //1213121
+        //121312141213121
 
     }
 }
