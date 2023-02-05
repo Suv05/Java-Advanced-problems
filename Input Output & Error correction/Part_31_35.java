4fd76c5aae862011d0670efb52fb55dc37291518
suvam <suvampravu@gmail.com>
2023-02-06 00:06:33 +0530

    Committing start from input-output question print from 36-40

--- Input Output & Error correction/Part_31_35.java
+++ Input Output & Error correction/Part_31_35.java
@@ -5,10 +5,29 @@ public class Part_31_35 {
 //        int x=-11;
 //        System.out.println(x%2);
 //        System.out.println(x/2);
-        //Q-31
-        //Q-31
-        //Q-31
-        //Q-31
+
+
+        //Q-32
+//        int 1stnum=10,nu-m2=20,3rd num=40;
+//        System.out.println("/"hello/"");
+//        byte b=128; float c=2.1; charc='a'; char cc=20;
+//        System.out.println(cc);
+
+
+        //Q-33
+//        int a = 10;
+//        System.out.println(a++++);
+
+        //Q-34
+//        int a=2;
+//        int b=4;
+//        System.out.println(“value of a XOR B:”+(a^b));
+
+        //Q-35
+//        int a = 10;
+//        if(++a==11 || ++a==12)
+//            ++a;
+//        System.out.println(a);
 
 
     }
---
+++ Input Output & Error correction/Part_36_40.java
@@ -0,0 +1,24 @@
+public class Part_36_40 {
+    public static void main(String[] args) {
+        //Q-36
+//        int a, b, result;
+//        a=10; b=20;
+//        result=(b>=a);
+//        System.out.println(result);
+        //Q-37
+//        int x=20;
+//        String sup = (x < 15) ? "small" : (x < 22)? "tiny" : "huge";
+//        System.out.println(sup);
+        //Q-38
+//        int a=12+21*3-9/2;
+//        int b=14-32*4+175/8-3;
+//        boolean p=(++a>71&&--b<20);
+//        System.out.println(p);
+//        boolean p1=(b-- == -99 || a-- > 100);
+//        System.out.println(p1);
+        //Q-39
+
+        //Q-36
+
+    }
+}
