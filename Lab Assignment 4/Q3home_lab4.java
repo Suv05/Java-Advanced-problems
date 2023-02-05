decb2ea486d76b308d09dbb0f54f2d0ebf96b090
suvam <suvampravu@gmail.com>
2023-02-06 02:40:00 +0530

    Lab 4 add committing answer

--- Lab Assignment 4/Q3home_lab4.java
+++ Lab Assignment 4/Q3home_lab4.java
@@ -2,5 +2,10 @@ public class Q3home_lab4 {
     public static void main(String[] args) {
 //Q-Write a java program in which using one for loop and one if statement, prints the integers
 //from 1,000 to 2,000 with five integers per line. Hint: Use the % operation.
+        int sum=0;
+        for (int i = 1000; i <=2000; i++) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
     }
 }
