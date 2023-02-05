8626e39a93d7985931d3e8433a0cdcb677d98d65
suvam <suvampravu@gmail.com>
2023-02-06 00:58:45 +0530

    Lab 4 add committing answer

--- Lab Assignment 4/Q10_lab4.java
+++ Lab Assignment 4/Q10_lab4.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Q10_lab4 {
     public static void main(String[] args) {
         //Q- Write a java program to print largest power of three less than or equal to N.
@@ -5,6 +6,16 @@ public class Q10_lab4 {
         //Sample run:
         //Enter a number: 100
         //The largest power of 3 less than or equal to 100 is 81
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int pow = 1;
+        for (int i = 1; i <= 5; i++) {
+            pow = 3*pow;
+            System.out.println(pow);
+
+        }
+            System.out.println("The largest power of 3 less than or equal to "+num+" is "+ pow);
 
     }
 }
