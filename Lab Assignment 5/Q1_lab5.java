9b77c57c91af677f6e73042099e0295e7bddd165
suvam <suvampravu@gmail.com>
2023-02-08 02:08:58 +0530

    commit lab 5 start

--- Lab Assignment 5/Q1_lab5.java
+++ Lab Assignment 5/Q1_lab5.java
@@ -1,5 +1,16 @@
 public class Q1_lab5 {
     public static void main(String[] args) {
+//Question-1:
+//Amicable numbers are a pair of numbers each of whose divisors are added to give the other number.
+//Example: The smallest pair of amicable numbers is (220, 284).They are amicable because the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and110, of which the sum is 284; and the proper divisors of 284 are 1, 2, 4, 71 and 142, of which the sum is 220.
 
+//Sample run 1:
+//Enter first number: 220
+//Enter second number: 284
+//220 and 284 are amicable numbers.
+//Sample run 2:
+//Enter first number: 220
+//Enter second number: 230
+//220 and 230 are not amicable numbers
     }
 }
