2dbc78bc3b9801272ee61d945376ce3b0caff1c0
suvam <suvampravu@gmail.com>
2023-02-08 20:09:26 +0530

    commit lab 5 Q-4 is half

--- Lab Assignment 5/Q4_lab5.java
+++ Lab Assignment 5/Q4_lab5.java
@@ -1,5 +1,21 @@
+import java.util.*;
 public class Q4_lab5 {
     public static void main(String[] args) {
+//Question-4:
+//WAP to calculate and display the factorial of all numbers between m and n (where m < n, m
+//> 0, n > 0)
 
+//Sample run:
+//Enter the value of m: 2
+//Enter the value of n: 5
+//Factorial of 2 is: 2
+//Factorial of 3 is: 6
+//Factorial of 4 is: 24
+//Factorial of 5 is: 120
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of m: ");
+        int m= sc.nextInt();
+        System.out.println("Enter the value of n: ");
+        int n= sc.nextInt();
     }
 }
