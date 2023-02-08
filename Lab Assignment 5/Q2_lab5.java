ca848a1b79d1406bf92cb91294eb189d7ad39180
suvam <suvampravu@gmail.com>
2023-02-08 18:27:07 +0530

    commit lab 5 Q-2 is complete🫠💯

--- Lab Assignment 5/Q2_lab5.java
+++ Lab Assignment 5/Q2_lab5.java
@@ -12,12 +12,34 @@ public class Q2_lab5 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int x = sc.nextInt();
+        //To check if it is a prim or not
+        boolean isPrime = true;
+        for (int i = 2; i <= x / 2; i++) {
+            if (x % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        //Revers of the number
         int r = 0;
         while (x != 0) {
             int digit = x % 10;
             r = r * 10 + digit;
-            x/=10;
+            x /= 10;
         }
-
+        //Check if it is prime or not
+        boolean isPrime2 = true;
+        for (int j = 2; j <= r / 2; j++) {
+            if (r % j == 0) {
+                isPrime2 = false;
+                break;
+            }
+        }
+        //To check if both are prime or not
+        if (isPrime2 == isPrime) {
+            System.out.println("It is twisted prime.");
+        }else {
+            System.out.println("It is not a twisted Prime");
+        }
     }
 }
