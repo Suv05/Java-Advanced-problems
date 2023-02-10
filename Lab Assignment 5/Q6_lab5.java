af374ebce2cb1c94d010432fc04ef36c4a32b2f8
suvam <suvampravu@gmail.com>
2023-02-10 23:30:55 +0530

    commit of Q-6 complete d

--- Lab Assignment 5/Q6_lab5.java
+++ Lab Assignment 5/Q6_lab5.java
@@ -34,20 +34,28 @@ public class Q6_lab5 {
         //4 5 6
         //7 8 9 10
         //11 12 13 14 15
-        int count = 1;
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(count + " ");
-                count++;
-            }
-            System.out.print("\n");
-        }
+//        int count = 1;
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(count + " ");
+//                count++;
+//            }
+//            System.out.print("\n");
+//        }
 
         //1
         //1 2
         //1 2 3
         //1 2 3 4
         //1 2 3 4 5
+
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println("\n");
+        }
+
 
 
     }
