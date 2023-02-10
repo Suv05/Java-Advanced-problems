bcd06e8b65d5fc747561b0d0e1773b6ecbbdab42
suvam <suvampravu@gmail.com>
2023-02-10 21:42:57 +0530

    commit of Q-6 complete a

--- Lab Assignment 5/Q6_lab5.java
+++ Lab Assignment 5/Q6_lab5.java
@@ -1,5 +1,31 @@
 public class Q6_lab5 {
     public static void main(String[] args) {
-//Q
+//Question-6:
+//Write a program to print the following outputs using for loops:
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+
+//        for (int i = 1; i < 6; i++) {
+//            for (int j = i; j >0; j--) {
+//
+//            System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+
+//1
+//2 2
+//3 3 3
+//4 4 4 4
+//5 5 5 5 5
+        for (int i = 1; i < 6; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
     }
 }
