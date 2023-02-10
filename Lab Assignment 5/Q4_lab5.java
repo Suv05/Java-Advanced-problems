fe673a728dab558449e822cf34cfbe71e0b88630
suvam <suvampravu@gmail.com>
2023-02-10 19:58:37 +0530

    commit lab 5 Q-4 is complete after long 25min  brain-twist

--- Lab Assignment 5/Q4_lab5.java
+++ Lab Assignment 5/Q4_lab5.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Q4_lab5 {
     public static void main(String[] args) {
 //Question-4:
@@ -14,9 +15,19 @@ public class Q4_lab5 {
 //Factorial of 5 is: 120
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of m: ");
-        int m= sc.nextInt();
+        int m = sc.nextInt();
         System.out.println("Enter the value of n: ");
-        int n= sc.nextInt();
+        int n = sc.nextInt();
+        int fact = 1;
+        for (int i = m; i <= n; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.println(j);
+                fact = fact * j;
+                break;
+            }
 
+            System.out.println("Factorial of " + i + " is: " + fact);
+
+        }
     }
 }
