8257da1e2b5096ed9259fa29ed7ced8147a05607
suvam <suvampravu@gmail.com>
2023-02-10 23:53:52 +0530

    commit of Q-7 complete a

--- Lab Assignment 5/Q7_lab5.java
+++ Lab Assignment 5/Q7_lab5.java
@@ -1,5 +1,17 @@
 public class Q7_lab5 {
     public static void main(String[] args) {
+//Question-7:
+        //A
+        //A B
+        //A B C
+        //A B C D
+        //A B C D E
 
+        for (int i = 1; i < 6; i++) {
+            for (int j = 65; j < 65 + i; j++) {
+                System.out.print((char)j+" ");
+            }
+            System.out.print("\n");
+        }
     }
 }
