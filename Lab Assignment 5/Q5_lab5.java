58154820d6463aa40f8fc506ee4360e2c225510f
suvam <suvampravu@gmail.com>
2023-02-10 20:01:26 +0530

    question commit of Q-5

--- Lab Assignment 5/Q5_lab5.java
+++ Lab Assignment 5/Q5_lab5.java
@@ -1,5 +1,29 @@
 public class Q5_lab5 {
     public static void main(String[] args) {
-//Q
+//Question-5:
+//WAP to display the multiplication table from 2 to 15.
+//Sample run:
+//Multiplication table of 2
+//2 × 1 = 2
+//2 × 2 = 4
+//:
+//:
+//2 × 10 = 20
+//Multiplication table of 3
+//3 × 1 = 3
+//3 × 2 = 6
+//:
+//:
+//3 × 10 = 30
+//:
+//:
+//:
+//Multiplication table of 15
+//15 × 1 = 15
+//15 × 2 = 30
+//:
+//:
+//15 × 10 = 150
+
     }
 }
