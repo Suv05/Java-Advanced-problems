60b6481c3f805e98a6d0095d6da8cdd60f960b8a
suvam <suvampravu@gmail.com>
2023-02-11 21:33:40 +0530

    commit of icp exam day

--- Input Output & Error correction/Part_26_30.java
+++ Input Output & Error correction/Part_26_30.java
@@ -4,6 +4,7 @@ public class Part_26_30 {
 //        int i = 132;
 //        short s = 15;
 //        byte b = (byte) i;
+//        System.out.println(b); //It is bcz byte range is from -128 to 127
 //        int x = b + s;
 //        System.out.println("Value of x is " + x);
 
@@ -18,7 +19,7 @@ public class Part_26_30 {
 
 
         //Q-28
-//        byte y=5,z=-y;
+//        byte y=5,z=  -y;
 //        System.out.println(~y);
 //        System.out.println(~z);
 //        y&= ~y;
@@ -32,7 +33,7 @@ public class Part_26_30 {
         //Q-29
 //        System.out.println(2!=3 && (7>8 || 6>5 ));
 //        System.out.println(!(2!=3) && (7>8 || 6>5 ));
-//        System.out.println(3==3 && z>=10 ));
+//        System.out.println(3==3 && z>=10 ));  //Error❌❌
 //        System.out.println(2!=3 && (7>8 || 6>5 ));
 
 
@@ -41,7 +42,7 @@ public class Part_26_30 {
 //        int v=10;
 //        System.out.println(v%=3*4);int x=11;
 //        System.out.println(-x- -); System.out.println(x);
-//        x = -x- -;
+//        x = -x- -;    //Error Excepted ❌❌❓
 //        System.out.println(x); int y = -x- -;
 //        System.out.println(x+""+y);
 
---
+++ Input Output & Error correction/Part_51_55.java
@@ -0,0 +1,71 @@
+public class Part_51_55 {
+    public static void main(String[] args) {
+        //Q-51
+//        int i = 1;
+//        switch (i) {
+//            case i:
+//                printf("case 1 executed");
+//                break;
+//            case i + 1
+//                ;
+//                printf("case 2 executed");
+//                break;
+//            default:
+//                printf("default block executed");
+//                break;
+//        }
+        //Q-52
+//        public class A {
+//            public final String xyz = "if";
+//
+//            public static void main(String[] args) {
+//                int a = 10, b = 5;
+//                xyz(a >> 1 == b)
+//                {
+//                    System.out.println("ABC");
+//                }
+//            }
+//        }
+
+        //Q-53
+//        int a = 10, b = 5;
+//        switch (a < b) {
+//            case true:
+//                System.out.println("Wow");
+//                break;
+//            case false:
+//                System.out.println("Its working");
+//                break;
+//        }
+
+        //Q-54
+//        public class A {
+//            public static final int x = 4;
+//
+//            public static void main(String[] args) {
+//                int a = 10, b = 5;
+//                switch (a < b ? a : b - 1) {
+//                    case 5:
+//                        System.out.println("Wow");
+//                        break;
+//                    case 3:
+//                        System.out.println("Its working");
+//                        break;
+//                    case x:
+//                        System.out.println("Ooh...");
+//                        break;
+//                    default:
+//                        System.out.println("Fine...");
+//                }
+//            }
+//        }
+        //Q-55
+//        int a = 10, b = 5;
+//        if (a < b) ;
+//        {
+//            if (b++ <= 5)
+//                System.out.println("Abc");
+//            System.out.println("Def");
+//        }
+    }
+}
