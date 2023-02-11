9a1a0e88dc5bc14d30edbc806d72be7480958202
suvam <suvampravu@gmail.com>
2023-02-11 21:33:02 +0530

    commit of icp exam day

--- Lab Assignment 5/Q7_lab5.java
+++ Lab Assignment 5/Q7_lab5.java
@@ -26,9 +26,22 @@ public class Q7_lab5 {
         //$ $ $
         //$ $
         //$
-        for (int i = 5; i >0; i--) {
-            for (int j = 1; j <=i; j++) {
-                System.out.print("$" + " ");
+//        for (int i = 5; i >0; i--) {
+//            for (int j = 1; j <=i; j++) {
+//                System.out.print("$" + " ");
+//            }
+//            System.out.print("\n");
+//        }
+
+        //            1
+        //         2  2
+        //      3  3  3
+        // 4   4  4   4
+        // 5  5  5  5 5
+
+        for (int i = 1; i < 6; i++) {
+            for (int j = 5; j > 0; j--) {
+                System.out.print(j+" ");
             }
             System.out.print("\n");
         }
