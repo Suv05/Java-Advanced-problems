46fdd68daed46f72e9971c719434b1a735361332
suvam <suvampravu@gmail.com>
2023-02-11 21:33:33 +0530

    commit of icp exam day

---
+++ Input Output & Error correction/Part_41_45.java
@@ -0,0 +1,54 @@
+public class Part_41_45 {
+    public static void main(String[] args) {
+        //Q-41
+//        if (1 < 2) {
+//            System.out.println("1 is less than 2");
+//        } else
+//            System.out.println("2 is less than 1");
+//        System.out.println("Hello");
+
+        //Q-42
+//        int a = 12 + 21 * 3 - 9 / 2;
+//        int b = 14 - 32 * 4 + 175 / 8 - 3;
+//        if (++a > 71 && --b < 20) {
+//            System.out.println("a = " + a + " b = " + b);
+//        }
+//        if (b-- == -97 || a-- < 100) {
+//            System.out.println("a = " + a + " b = " + b);
+//        }
+
+        //Q-43
+//        int a = 10;
+//        if (10L == a)
+//            System.out.println("10L");
+//        if (10 == a)
+//            System.out.println("10");
+//        else
+//            System.out.println("0");
+
+        //Q-44
+//        int x = 'a';
+//        switch (x) {
+//            case 5:
+//                x += 5;
+//            case 97:
+//                x += 10;
+//            case 98:
+//                x += 15;
+//                break;
+//            case 99:
+//                x += 20;
+//        }
+//        System.out.println(x);
+
+        //Q-45
+//        float a = 0.7f;
+//        if (a < 0.7)
+//            System.out.println("Hello World");
+//        else
+//            System.out.println("Hello ITER ");
+//    }
+
+
+}
+}
---
+++ Input Output & Error correction/Part_46_50.java
@@ -0,0 +1,51 @@
+public class Part_46_50 {
+    public static void main(String[] args) {
+        //Q-46
+//        int a = 35;
+//        int b = 25;
+//        if ((a > b) && (a = 5) < 15)
+//            System.out.println(a);
+//        else
+//            System.out.println(b);
+
+        //Q-47
+//        double x = 6.2;
+//        if (x-- >= 6.0)
+//            System.out.print("first ");
+//        if (--x >= 5.0)
+//            System.out.print("second ");
+//        if (x-- >= 4.0)
+//            System.out.print("third ");
+//        else
+//            System.out.print("fourth ");
+
+        //Q-48
+//        switch (*(3 + "I LOVE" "ABCD" + 3))
+//        {
+//            case 'A':
+//                System.out.println("Apple Mac");
+//                break;
+//            case 'B':
+//                System.out.println("Windows");
+//                break;
+//            case 'C':
+//                System.out.println("Great Linux");
+//                break;
+//            default:
+//                System.out.println("All the above");
+
+        //Q-49
+//        if (!(System.out.println("x")))
+//            System.out.println("if part");
+//        else
+//            System.out.println(" else part");
+        //Q-50
+//        if ( int q = 0)
+//        System.out.println("if part");
+//else
+//        System.out.println("else part");
+//        return 0;
+
+
+    }
+}
