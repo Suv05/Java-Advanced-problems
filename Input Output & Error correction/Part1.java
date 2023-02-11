73afe26dee3d32259ae1f42830bd98577ddc8894
suvam <suvampravu@gmail.com>
2023-02-11 21:33:53 +0530

    commit of icp exam day

--- Input Output & Error correction/Part1.java
+++ Input Output & Error correction/Part1.java
@@ -1,10 +1,10 @@
 public class Part1 {
     public static void main(String[] args) {
         //Q1-
-        // System.out.println(System.out.println("hi"));
+//         System.out.println(System.out.println("hi"));  Error❌
 
         //Q4-
-        //System.out.print(013 + 1.94 + "C" + "S");
+//        System.out.print(013 + 1.94 + "C" + "S");
         //Here 013 is the octal of Decimal 11.Decimal-> Octal Integer.toOctalString(int num)
         //Also see the octal to decimal conversion ❤️🫡
 
--- Input Output & Error correction/Part5_9.java
+++ Input Output & Error correction/Part5_9.java
@@ -15,10 +15,12 @@ public class Part5_9 {
         System.out.println(x >> 2);
         int y = -5;
         System.out.println(y << 2);//but +ve can sift till 0
+        int z=4;
+        System.out.println(z>>>2);
 
         //Q-7
-        System.out.println(10 + 15 + "Hello");
-        System.out.println("Hello" + 10 + 15);
+//        System.out.println(10 + 15 + "Hello");
+//        System.out.println("Hello" + 10 + 15);
 
         //Q-8
 //        byte b = 10;
@@ -26,9 +28,9 @@ public class Part5_9 {
 //        System.out.println(b);
 
         //Q-9
-        int i = 4;
-        int j = 21;
-        int k = ++i * 7 + 2 - j--;  //++4= 5,5*7=35+2=37, 37-21=16,
-        System.out.println("k = " + k);
+//        int i = 4;
+//        int j = 21;
+//        int k = ++i * 7 + 2 - j--;  //++4= 5,5*7=35+2=37, 37-21=16,
+//        System.out.println("k = " + k);
     }
 }
