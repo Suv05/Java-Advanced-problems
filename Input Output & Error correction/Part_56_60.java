06415a67e2616ac227bbe95a4f1f077257ee163d
suvam <suvampravu@gmail.com>
2023-02-11 21:33:27 +0530

    commit of icp exam day

---
+++ Input Output & Error correction/Part_56_60.java
@@ -0,0 +1,76 @@
+public class Part_56_60 {
+    public static void main(String[] args) {
+        //Q-56
+//        int a = 1024, b = 1024;
+//        boolean c;
+//        if (c = a >> 9 == (b / Math.pow(2, 9))) {
+//            System.out.println("HELLO");
+//        } else {
+//            System.out.println("BYE");
+//        }
+        //Q-57
+//        int a = 10, b = 20, c = 30;
+//        if (c > b > a) {
+//            System.out.println("TRUE");
+//        } else {
+//            System.out.println("FALSE");
+//        }
+        //Q-58
+//        int N = 3;
+//        switch (N) {
+//            case 1:
+//                System.out.println("The number is 1.");
+//                break;
+//            case 2:
+//            case 4:
+//            case 8:
+//                System.out.println("The number is 2, 4, or 8.");
+//                System.out.println("(That’s a power of 2!)");
+//                break;
+//            case 3:
+//            case 6:
+//            case 9:
+//                System.out.println("The number is 3, 6, or 9.");
+//                System.out.println("(That’s a multiple of 3!)");
+//                break;
+//            case 5:
+//                System.out.println("The number is 5.");
+//                break;
+//            default:
+//                System.out.println("The number is 7 or is outside the range1 to 9. ");
+//        }
+        //Q-59
+//        String branch = “ECE”;
+//        int semester = 1;
+//        switch (semester) {
+//            case 1:
+//                System.out.println(”ICP - CSE 1001”);
+//                switch (branch) {
+//                    case “ECE”:
+//                        System.out.prinltln(“ECE is learning ICP”);
+//                    case “CSE”:
+//                    case “EE”:
+//                        System.out.println(“EE is learning ICP”);
+//                }
+//            case 2:
+//            case 3:
+//            case 1:
+//            default:
+//                System.out.println(“Default”);
+
+        //Q-60
+//        int number1 = 111, number2 = 101;
+//        switch (number1 & number2) {
+//            case 1:
+//                System.out.println("-1-");
+//            case 101:
+//                System.out.println("-101-");
+//            case 111:
+//                System.out.println("-111-");
+//            case 010:
+//                System.out.println("-010-");
+//            default:
+//                System.out.println("-" + x & y + "-");
+//        }
+    }
+}
