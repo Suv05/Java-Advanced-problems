3bd094bde8b33338b3395a1df0bca0ce80e2dc69
suvam <suvampravu@gmail.com>
2023-02-11 21:33:10 +0530

    commit of icp exam day

--- Lab Assignment 4/Q4_lab4.java
+++ Lab Assignment 4/Q4_lab4.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Q4_lab4 {
     public static void main(String[] args) {
 //Q-Write a program that finds the greatest common divisor (GCD) of two numbers using Euclid's
@@ -8,5 +9,20 @@ public class Q4_lab4 {
 //Enter the first number: 56
 //Enter the second number: 98
 //GCD of 56 and 98 is 14
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+        int gcd;
+
+        while (num2 != 0) {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+
+        gcd = num1;
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is " + gcd);
     }
 }
--- Lab assignment 1/Q5Home_lab1.java
+++ Lab assignment 1/Q5Home_lab1.java
@@ -1,5 +1,27 @@
 public class Q5Home_lab1 {
     public static void main(String[] args) {
 
+        // Using extra variables
+        int W, A, K, G;
+        int temp1 = W;
+        int temp2 = A;
+        int temp3 = K;
+        W = G;
+        A = temp3;
+        K = temp2;
+        G = temp1;
+
+// Without using extra variables
+        W = W + G;
+        G = W - G;
+        W = W - G;
+
+        A = A + K;
+        K = A - K;
+        A = A - K;
+
+// Now the values of W, G, K, and A have been exchanged as required.
+
+
     }
 }
