9adf5385b0bd04a80ee79e0ac213bd371002543d
suvam <suvampravu@gmail.com>
2023-02-21 22:19:19 +0530

    Lab assignment new commit

--- Lab assignment 1/Q5Home_lab1.java
+++ Lab assignment 1/Q5Home_lab1.java
@@ -1,8 +1,10 @@
+import java.io.FilterOutputStream;
+
 public class Q5Home_lab1 {
     public static void main(String[] args) {
 
         // Using extra variables
-        int W, A, K, G;
+        int W = 1, A = 2, K = 3, G = 4;
         int temp1 = W;
         int temp2 = A;
         int temp3 = K;
@@ -10,17 +12,24 @@ public class Q5Home_lab1 {
         A = temp3;
         K = temp2;
         G = temp1;
-
+        System.out.println("W = "+W);
+        System.out.println("A = "+A);
+        System.out.println("K = "+K);
+        System.out.println("G = "+G);
 // Without using extra variables
-        W = W + G;
-        G = W - G;
-        W = W - G;
+//        W = W + G;
+//        G = W - G;
+//        W = W - G;
+//
+//        A = A + K;
+//        K = A - K;
+//        A = A - K;
+//        System.out.println("W = "+W);
+//        System.out.println("A = "+A);
+//        System.out.println("K = "+K);
+//        System.out.println("G = "+G);
 
-        A = A + K;
-        K = A - K;
-        A = A - K;
-
-// Now the values of W, G, K, and A have been exchanged as required.
+// Now the values of W, G, K, & A have been exchanged as required.
 
 
     }
