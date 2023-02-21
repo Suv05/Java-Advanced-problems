54032e5e6fc3082b97487bc3d21265657785db2c
suvam <suvampravu@gmail.com>
2023-02-21 23:46:40 +0530

    commit this

--- Lab assignment 1/Q3home_lab1.java
+++ Lab assignment 1/Q3home_lab1.java
@@ -1,13 +1,14 @@
 public class Q3home_lab1 {
     public static void main(String[] args) {
-        int a = 2147483647;
+//        int a = 2147483647;
+        int a = Integer.MAX_VALUE;
         System.out.println(a);
         System.out.println(a + 1);
         System.out.println(2 - a);
         System.out.println(-2 - a);
         System.out.println(2 * a);
-        System.out.println(3 * a);
         System.out.println(4 * a);
+        System.out.println(3 * a);
         System.out.println(5 * a);
         System.out.println(6 * a);
     }
