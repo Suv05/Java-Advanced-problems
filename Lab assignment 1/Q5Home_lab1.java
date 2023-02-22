50e5b249c78bbb833757fdccc792e0ab6121b7a4
suvam <suvampravu@gmail.com>
2023-02-22 18:24:43 +0530

    commit this

--- Lab assignment 1/Q5Home_lab1.java
+++ Lab assignment 1/Q5Home_lab1.java
@@ -3,29 +3,29 @@ public class Q5Home_lab1 {
 
         // Using extra variables
         int W = 1, A = 2, K = 3, G = 4;
-        int temp1 = W;
-        int temp2 = A;
-        int temp3 = K;
-        W = G;
-        A = temp3;
-        K = temp2;
-        G = temp1;
-        System.out.println("W = "+W);
-        System.out.println("A = "+A);
-        System.out.println("K = "+K);
-        System.out.println("G = "+G);
-// Without using extra variables
-//        W = W + G;
-//        G = W - G;
-//        W = W - G;
-//
-//        A = A + K;
-//        K = A - K;
-//        A = A - K;
+//        int temp1 = W;
+//        int temp2 = A;
+//        int temp3 = K;
+//        W = G;
+//        A = temp3;
+//        K = temp2;
+//        G = temp1;
 //        System.out.println("W = "+W);
 //        System.out.println("A = "+A);
 //        System.out.println("K = "+K);
 //        System.out.println("G = "+G);
+// Without using extra variables
+        W = W + G;
+        G = W - G;
+        W = W - G;
+
+        A = A + K;
+        K = A - K;
+        A = A - K;
+        System.out.println("W = "+W);
+        System.out.println("A = "+A);
+        System.out.println("K = "+K);
+        System.out.println("G = "+G);
 
 // Now the values of W, G, K, & A have been exchanged as required.
 
