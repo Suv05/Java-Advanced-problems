41560c2594640a0a57c143cc033e2c46641bb2bf
suvam <suvampravu@gmail.com>
2023-02-22 22:03:37 +0530

    commit new version of simpled solution of problem💯❤️‍🔥

--- Lab Assignment 3/Q7_lab3.java
+++ Lab Assignment 3/Q7_lab3.java
@@ -19,9 +19,6 @@ public class Q7_lab3 {
         char choice = sc.next().charAt(0);
         if (choice == 'Y' || choice == 'y') {
             System.out.println("Here is your bill");
-        } else {
-            System.out.println("Pay bill in offline then");
-        }
             double t1, t2, t3, t4;
             t1 = unit * 3;
             t2 = 50 * 3 + (unit - 50) * 4.80;
@@ -29,28 +26,32 @@ public class Q7_lab3 {
             t4 = (400 * 5.80) + (unit - 400) * 6.20;
 
             double pay1, pay2, pay3, pay4;
-            pay1 = t1-(t1 * 3) / 100;
+            pay1 = t1 - (t1 * 3) / 100;
             pay2 = t2 - (t2 * 3) / 100;
             pay3 = t3 - (t3 * 3) / 100;
             pay4 = t4 - (t4 * 3) / 100;
+
             if (unit < 50) {
                 System.out.println("Your bill amount " + t1);
                 System.out.println("Your discount is " + (t1 * 3) / 100);
-                System.out.println("Amount payable is: "+ pay1);
+                System.out.println("Amount payable is: " + pay1);
             } else if (unit > 50 && unit <= 200) {
                 System.out.println("Your bill amount " + t2);
                 System.out.println("Your discount is " + (t2 * 3) / 100);
-                System.out.println("Amount payable is: "+ pay2);
+                System.out.println("Amount payable is: " + pay2);
             } else if (unit > 200 && unit <= 400) {
                 System.out.println("Your bill amount " + t3);
                 System.out.println("Your discount is " + (t3 * 3) / 100);
-                System.out.println("Amount payable is: "+ pay3);
+                System.out.println("Amount payable is: " + pay3);
             } else {
                 System.out.println("Your bill amount " + t4);
                 System.out.println("Your discount is " + (t4 * 3) / 100);
-                System.out.println("Amount payable is: "+ pay4);
+                System.out.println("Amount payable is: " + pay4);
             }
-
+        } else {
+            System.out.println("Pay bill in offline then");
         }
+
     }
+}
 
