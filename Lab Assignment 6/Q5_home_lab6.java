dbdcff1ab73289a2dbf26c7e919a6a2d24d72452
suvam <suvampravu@gmail.com>
2023-02-26 17:19:30 +0530

    commit assignment 6.1 complete

--- Lab Assignment 6/Q5_home_lab6.java
+++ Lab Assignment 6/Q5_home_lab6.java
@@ -2,7 +2,7 @@ public class Q5_home_lab6 {
     public static boolean areConsecutive(int num1, int num2, int num3) {
         // sort the numbers in ascending order
         int[] nums = { num1, num2, num3 };
-        Arrays.sort(nums);
+//        Arrays.sort(nums);
 
         // check if the numbers are consecutive
         return nums[0] + 1 == nums[1] && nums[1] + 1 == nums[2];
@@ -11,7 +11,7 @@ public class Q5_home_lab6 {
     int num2 = 6;
     int num3 = 7;
 
-    boolean result = areConsecutive(num1, num2, num3);
-System.out.println(result); // prints true
+//    boolean result = areConsecutive(num1, num2, num3);
+//System.out.println(result); // prints true
 
 }
