2a9e41e03ecf3de19cd7973dc50b7f9df985ccfe
suvam <suvampravu@gmail.com>
2023-02-26 18:11:22 +0530

    commit assignment 6.3 complete

--- Lab Assignment 6/Q3_Lab6.java
+++ Lab Assignment 6/Q3_Lab6.java
@@ -1,28 +1,27 @@
-    import java.util.Scanner;
+import java.util.Scanner;
 public class Q3_Lab6 {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-            System.out.print("Enter an integer: ");
-            int number = input.nextInt();
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
 
-            if (isPalindrome(number)) {
-                System.out.println(number + " is a palindrome");
-            } else {
-                System.out.println(number + " is not a palindrome");
-            }
-        }
-
-        public static boolean isPalindrome(int number) {
-            int reverse = 0;
-            int original = number;
-
-            while (number != 0) {
-                int digit = number % 10;
-                reverse = reverse * 10 + digit;
-                number /= 10;
-            }
-
-            return reverse == original;
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome");
+        } else {
+            System.out.println(number + " is not a palindrome");
         }
     }
+
+    public static boolean isPalindrome(int x) {
+        int reverse = 0;
+        int original = x;
+
+        while (x != 0) {
+            reverse = reverse * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return reverse == original;
+    }
+}
