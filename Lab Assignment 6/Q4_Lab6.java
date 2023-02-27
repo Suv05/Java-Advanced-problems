a256ee89b89ae805384a35326be96a3f5aa625f8
suvam <suvampravu@gmail.com>
2023-02-28 01:33:20 +0530

    commit assignment 6.5 complete

--- Lab Assignment 6/Q4_Lab6.java
+++ Lab Assignment 6/Q4_Lab6.java
@@ -1,4 +1,5 @@
 public class Q4_Lab6 {
+    //Write a java program that displays the number of days in year from 2000 to 2020.
     public static void main(String[] args) {
         for (int year = 2000; year <= 2020; year++) {
             int days = getDaysInYear(year);
