585685e552517874f23557949ecbc7ac29f225a4
suvam <suvampravu@gmail.com>
2023-02-28 02:15:52 +0530

    commit assignment 6.7 complete

--- Lab Assignment 6/Q8_Lab6.java
+++ Lab Assignment 6/Q8_Lab6.java
@@ -1,4 +1,14 @@
 public class Q8_Lab6 {
+    //Write a java method to check a string is palindrome or not.
+    public static void main(String[] args) {
+        String inputString = "A man, a plan, a canal: Panama";
+        if (isPalindrome(inputString)) {
+            System.out.println(inputString + " is a palindrome!");
+        } else {
+            System.out.println(inputString + " is not a palindrome.");
+        }
+    }
+
     public static boolean isPalindrome(String inputString) {
         // Remove all non-alphanumeric characters and convert to lowercase
         String cleanString = inputString.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
@@ -16,11 +26,5 @@ public class Q8_Lab6 {
 
         return true;
     }
-//    String inputString = "A man, a plan, a canal: Panama";
-//if (isPalindrome(inputString)) {
-//        System.out.println(inputString + " is a palindrome!");
-//    } else {
-//        System.out.println(inputString + " is not a palindrome.");
-//    }
 
 }
