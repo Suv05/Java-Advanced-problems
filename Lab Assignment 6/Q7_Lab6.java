032ef3f4377d53dc921f049227924fa880e4acd2
suvam <suvampravu@gmail.com>
2023-02-28 01:57:28 +0530

    commit assignment 6.7 complete

--- Lab Assignment 6/Q7_Lab6.java
+++ Lab Assignment 6/Q7_Lab6.java
@@ -1,4 +1,15 @@
 public class Q7_Lab6 {
+    //Write java method called count accepts a string as input and returns the number of vowels in
+    //it. The method header is given below.
+
+    //For example, count ("Welcome") returns 2.
+    public static void main(String[] args) {
+        String inputString = "Hello, world!";
+        int vowelCount = countVowels(inputString);
+        System.out.println("The number of vowels in the input string is " + vowelCount);
+
+    }
+
     public static int countVowels(String inputString) {
         int vowelCount = 0;
         String vowels = "aeiouAEIOU"; // A string containing all vowels in uppercase and lowercase
@@ -12,8 +23,4 @@ public class Q7_Lab6 {
 
         return vowelCount;
     }
-//    String inputString = "Hello, world!";
-//    int vowelCount = countVowels(inputString);
-//System.out.println("The number of vowels in the input string is " + vowelCount);
-
 }
