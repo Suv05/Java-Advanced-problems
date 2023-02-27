6756224f4bc466e4531504764ca43ee8fe30c70f
suvam <suvampravu@gmail.com>
2023-02-28 01:33:29 +0530

    commit assignment 6.5 complete

--- Lab Assignment 6/Q2_lab6.java
+++ Lab Assignment 6/Q2_lab6.java
@@ -1,4 +1,13 @@
 public class Q2_lab6 {
+    //Question-2:
+    //A pentagonal number is defined as n (3n–1)/2 for n = 1, 2, . . ., and so on. Therefore, the first
+    //few numbers are 1, 5, 12, 22, . . . .
+
+    //Write a method with the following header that returns a pentagonal number:
+
+    //Write a java program that uses this method to display the first 100 pentagonal numbers with
+    //10 numbers on each line.
+
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             System.out.printf("%-7d", getPentagonalNumber(i));
