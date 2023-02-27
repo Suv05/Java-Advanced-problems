fa18fbfd059af2ee7ea5af0c808eaa27350addf0
suvam <suvampravu@gmail.com>
2023-02-28 02:16:01 +0530

    commit assignment 6.7 complete

--- Lab Assignment 6/Q9_Lab6.java
+++ Lab Assignment 6/Q9_Lab6.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
-
 public class Q9_Lab6 {
+    //Write a method that checks whether a string is a valid password. Suppose the password rules
+    //are as follows:
+    // A password must have at least eight characters.
+    // A password consists of only letters and digits.
+    // A password must contain at least two digits.
+
+    //Write a program that prompts the user to enter a password and displays Valid Password if the
+    //rules are followed or Invalid Password otherwise.
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a password: ");
