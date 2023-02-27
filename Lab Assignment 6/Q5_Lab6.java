ce248273cf6a35fbd85f83a90464320ac17ac144
suvam <suvampravu@gmail.com>
2023-02-28 01:28:37 +0530

    commit assignment 6.5 complete

--- Lab Assignment 6/Q5_Lab6.java
+++ Lab Assignment 6/Q5_Lab6.java
@@ -1,12 +1,16 @@
 public class Q5_Lab6 {
+    public static void main(String[] args) {
+        double area = getAreaOfRegularPolygon(6, 10.0);
+        System.out.println("The area of the regular hexagon is " + area);
+    }
+
     public static double getAreaOfRegularPolygon(int n, double sideLength) {
-        double apothem = sideLength / (2 * Math.tan(Math.PI / n));
-        double perimeter = n * sideLength;
-        double area = 0.5 * apothem * perimeter;
+        double a = n * Math.pow(sideLength, 2);
+        double b = 4 * Math.tan(Math.PI / n);
+        double area = a / b;
         return area;
     }
-    //double area = getAreaOfRegularPolygon(6, 10.0);
-//System.out.println("The area of the regular hexagon is "+area);
+
 
     //area = 0.5 * apothem * perimeter
     //apothem = s / (2 * tan(pi/n))
