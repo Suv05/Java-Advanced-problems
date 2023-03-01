d3d62a34c058f30fb11c20ef5a2435be7d3fc3d8
suvam <suvampravu@gmail.com>
2023-03-02 00:40:40 +0530

    commit array question 4

--- 1-D Arrays/Q4_Array.java
+++ 1-D Arrays/Q4_Array.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Q4_Array {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        double[] numbers = new double[10];
+
+        System.out.print("Enter ten numbers: ");
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = scanner.nextDouble();
+        }
+
+        double min = findMinimum(numbers);
+        System.out.printf("The minimum number is: %.1f", min);
+    }
+
+    public static double findMinimum(double[] arr) {
+        double min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+
+                min = arr[i];
+            }
+        }
+        return min;
     }
 }
