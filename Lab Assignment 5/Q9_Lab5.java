ed28c43c8a44d305b20bf1c7b90d6507672fed5b
suvam <suvampravu@gmail.com>
2023-03-02 00:41:21 +0530

    commit 9

---
+++ Lab Assignment 5/Q9_Lab5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Q9_Lab5 {
+    //Write a program that will read the value of n from the user and calculate sum of the following
+    //series: 1/1^2 + 1/2^2 + 1/3^2 + ... + 1/n^2
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / (i * i);
+        }
+
+        System.out.println("Sum of the series is: " + sum);
+    }
+}
