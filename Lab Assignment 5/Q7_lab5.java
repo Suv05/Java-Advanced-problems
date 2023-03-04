513050d25760ae5b7da0a00855a0415e87d61cff
suvam <suvampravu@gmail.com>
2023-03-04 22:21:42 +0530

    commit status

--- Lab Assignment 5/Q7_lab5.java
+++ Lab Assignment 5/Q7_lab5.java
@@ -7,18 +7,19 @@ public class Q7_lab5 {
         //A B C D
         //A B C D E
 
-//        for (int i = 1; i < 6; i++) {
-//            for (int j = 65; j < 65 + i; j++) {
-//                System.out.print((char)j+" ");
-//            }
-//            System.out.print("\n");
-//
-//        for (int i = 1; i < 5; i++) {
-//            for (int j = 0; j <= i; j++) {
-//                System.out.print((char) (65 + j) + " ");  //Method 2 for above 🪂🚀
-//            }
-//            System.out.println("\n");
-//        }
+        for (int i = 1; i < 6; i++) {
+            for (int j = 65; j < 65 + i; j++) {
+                System.out.print((char) j + " ");
+            }
+            System.out.print("\n");
+        }
+
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (65 + j) + " ");  //Method 2 for above 🪂🚀
+            }
+            System.out.println("\n");
+        }
 
 
         //$ $ $ $ $
@@ -26,12 +27,12 @@ public class Q7_lab5 {
         //$ $ $
         //$ $
         //$
-//        for (int i = 5; i >0; i--) {
-//            for (int j = 1; j <=i; j++) {
-//                System.out.print("$" + " ");
-//            }
-//            System.out.print("\n");
-//        }
+        for (int i = 5; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("$" + " ");
+            }
+            System.out.print("\n");
+        }
 
         //            1
         //         2  2
@@ -41,7 +42,7 @@ public class Q7_lab5 {
 
         for (int i = 1; i < 6; i++) {
             for (int j = 5; j > 0; j--) {
-                System.out.print(j+" ");
+                System.out.print(j + " ");
             }
             System.out.print("\n");
         }
