e789152b2dda934961ec00f02646f7abe68e7b35
suvam <suvampravu@gmail.com>
2023-03-08 19:48:44 +0530

    commit changes

--- Lab Assignment 5/Q6_lab5.java
+++ Lab Assignment 5/Q6_lab5.java
@@ -38,7 +38,7 @@ public class Q6_lab5 {
 
         for (int i = 1; i < 6; i++) {
             for (int j =1 ; j<=i; j++) {
-                System.out.print(i);
+                System.out.print(i+" ");
             }
             System.out.print("\n");
         }
