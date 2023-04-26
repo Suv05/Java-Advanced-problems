d340c04a6cfd7d7c1acbe9e3f0dd88ea237991e2
suvam <suvampravu@gmail.com>
2023-04-27 00:30:20 +0530

    commit DSA assignment 1

---
+++ DSA Assignment-1/Q_1.java
@@ -0,0 +1,26 @@
+/*Question-1:
+Write a Java program that can take a positive integer greater than 2 as input and write out the
+number of times one must repeatedly divide this number by 2 before getting a value less
+than 2.
+Sample Run:
+The positive integer greater than 2 from command line argument
+is 67.
+The number of times one must repeatedly divide this number by 2
+before getting a value less than 2 is 5.*/
+
+import java.util.*;
+public class Q_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int n = sc.nextInt();
+        int count = 0;
+//        for (int i=2;i<n;i++){
+//            if (n / 2) {
+//                count++;
+//            }
+//
+//
+//        }
+    }
+}
