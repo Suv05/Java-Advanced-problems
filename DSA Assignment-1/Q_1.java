8f77359ab5444bf7f5522e4a92c39de521121242
suvam <suvampravu@gmail.com>
2023-04-27 18:46:55 +0530

    commit Answer

--- DSA Assignment-1/Q_1.java
+++ DSA Assignment-1/Q_1.java
@@ -9,18 +9,18 @@ The number of times one must repeatedly divide this number by 2
 before getting a value less than 2 is 5.*/
 
 import java.util.*;
+
 public class Q_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your number");
+        System.out.println("Enter a positive integer greater than 2: ");
         int n = sc.nextInt();
         int count = 0;
-//        for (int i=2;i<n;i++){
-//            if (n / 2) {
-//                count++;
-//            }
-//
-//
-//        }
+        while (n > 2) {
+            n = n / 2;
+            count++;
+        }
+        System.out.println("Numbers of time it can be divided by 2 is: " + count);
+
     }
 }
---
+++ DSA Assignment-2/Q_3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class student{
+    int roll,DSA_Mark;
+    String name;
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getDSA_Mark() {
+        return DSA_Mark;
+    }
+    public void setDSA_Mark(int DSA_Mark) {
+        this.DSA_Mark = DSA_Mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+public class Q_3 {
+    public static void main(String[] args) {
+
+    }
+
+}
