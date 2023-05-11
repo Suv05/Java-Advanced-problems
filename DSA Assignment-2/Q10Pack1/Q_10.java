7bbd7ae24d03b4b512c97755b522dd7303f0501b
suvam <suvampravu@gmail.com>
2023-05-12 00:04:21 +0530

    commit the DSA-2 assignment

--- DSA Assignment-2/Q10Pack1/Q_10.java
+++ DSA Assignment-2/Q10Pack1/Q_10.java
@@ -1,5 +1,15 @@
-package Q10Pack1;
+//Create 2 package Q10Pack1 and Q10Pack2
+//create Q_10.java in pack1 and sports.java in Pack2
 
+//This is the 1st part of pack1 and Q_10.java
+package Q10Pack1;
+import java.util.Scanner;
+class Student{
+
+}
+class Test{
+
+}
 public class Q_10 {
     public static void main(String[] args) {
 
