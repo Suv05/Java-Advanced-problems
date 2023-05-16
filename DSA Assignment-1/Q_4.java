7f5a565a7fb04103f6108d2be83edb1e490dbb53
suvam <suvampravu@gmail.com>
2023-05-17 01:00:23 +0530

    commit my changes

--- DSA Assignment-1/Q_4.java
+++ DSA Assignment-1/Q_4.java
@@ -1,3 +1,13 @@
+/*Write a Java program that outputs all possible strings formed by using the characters 'c', 'a', 'r',
+'b', 'o', and 'n' exactly once.
+Example:
+For 3 characters ‘C’, ‘A’, ‘R’, the possible strings are
+CAR
+CRA
+RCA
+RAC
+ACR
+ARC */
 public class Q_4 {
     public static void main(String[] args) {
 
