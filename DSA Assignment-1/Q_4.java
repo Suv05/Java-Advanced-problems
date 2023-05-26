36131cb7c4b9b5b4a36362e3d57c336a488ca93b
suvam <suvampravu@gmail.com>
2023-05-26 18:52:44 +0530

    commit my changes

--- DSA Assignment-1/Q_4.java
+++ DSA Assignment-1/Q_4.java
@@ -1,7 +1,7 @@
-/*Write a Java program that outputs all possible strings formed by using the characters 'c', 'a', 'r',
+/*Write a Java program that outputs all possible strings formed by using the character 'c', 'a', 'r',
 'b', 'o', and 'n' exactly once.
 Example:
-For 3 characters ‘C’, ‘A’, ‘R’, the possible strings are
+For 3 character ‘C’, ‘A’, ‘R’, the possible strings are
 CAR
 CRA
 RCA
@@ -11,9 +11,9 @@ ARC */
 
 public class Q_4 {
 
-    static char[] characters = {'c', 'a', 'r', 'b', 'o', 'n'};
+    static char[] ch = {'c', 'a', 'r', 'b', 'o', 'n'};
 
-    private static void printPermutation(char[] a, int startIndex, int endIndex) {
+    public static void printPermutation(char[] a, int startIndex, int endIndex) {
         if (startIndex == endIndex)//reached end of recursion, print the state of a
             System.out.println(new String(a));
         else {
@@ -27,13 +27,13 @@ public class Q_4 {
         }
     }
 
-    private static void swap(char[] a, int i, int x) {
+    public static void swap(char[] a, int i, int x) {
         char t = a[i];
         a[i] = a[x];
         a[x] = t;
     }
 
     public static void main(String[] args) {
-        printPermutation(characters, 0, characters.length);
+        printPermutation(ch, 0, ch.length);
     }
 }
