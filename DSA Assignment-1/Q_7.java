60e129645e7bfe62ece2b3eda00f221b8d66984f
suvam <suvampravu@gmail.com>
2023-06-02 00:01:47 +0530

    commit my changes

--- DSA Assignment-1/Q_7.java
+++ DSA Assignment-1/Q_7.java
@@ -6,16 +6,16 @@ import java.util.*;
 
 public class Q_7 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of elements in the array: ");
-        int n = scanner.nextInt();
+        int n = sc.nextInt();
 
         int[] arr = new int[n];
 
         System.out.print("Enter the elements of the array: ");
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            arr[i] = sc.nextInt();
         }
 
         int max = arr[0];
