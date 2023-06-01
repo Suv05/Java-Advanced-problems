98f0605ce8d0a95c2d81f385f65adc6ca293abbf
suvam <suvampravu@gmail.com>
2023-06-02 03:12:13 +0530

    commit the DSA-2 assignment

--- DSA Assignment-2/Q_1.java
+++ DSA Assignment-2/Q_1.java
@@ -1,53 +1,49 @@
-import java.util.*;
 /*Define a class Phone to store these three parts of a phone number separately as instance
 variable (area_code, exchange, number). The class consists of two member methods: input ( )
 and display ( ). Create two class objects of type phone. Initialize one directly (by using dot (.)
 operator and instance variable with value area_code: 212, exchange: 767,
 number: 8900), and initialize other from the user through member method input ( ). Display
 both the numbers.*/
-class phone {
-    int area_code, exchange, number;
+import java.util.Scanner;
 
-    public int DisplayAreaCode() {
-        return area_code;
-    }
+class Phone {
+    private int areaCode;
+    private int exchange;
+    private int number;
 
-    public void inputAreaCode(int area_code) {
-        this.area_code = area_code;
-    }
-
-    public int DisplayExchange() {
-        return exchange;
-    }
-
-    public void inputExchange(int exchange) {
+    public Phone(int areaCode, int exchange, int number) {
+        this.areaCode = areaCode;
         this.exchange = exchange;
-    }
-
-    public int DisplayNumber() {
-        return number;
-    }
-
-    public void inputNumber(int number) {
         this.number = number;
+    }
+
+    public Phone() {
+        input();
+    }
+
+    public void input() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter area code: ");
+        areaCode = scanner.nextInt();
+        System.out.print("Enter exchange: ");
+        exchange = scanner.nextInt();
+        System.out.print("Enter number: ");
+        number = scanner.nextInt();
+    }
+
+    public void display() {
+        System.out.println("(" + areaCode + ") " + exchange + "-" + number);
     }
 }
 
 public class Q_1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your area_code ");
-        int x = sc.nextInt();
-        System.out.println("Enter your exchange");
-        int y = sc.nextInt();
-        System.out.println("Enter your number");
-        int z = sc.nextInt();
+        Phone phone1 = new Phone(212, 767, 8900);
+        Phone phone2 = new Phone();
 
-        phone ph = new phone();
-        ph.inputAreaCode(x);
-        ph.inputExchange(y);
-        ph.inputNumber(z);
-        System.out.println("Your phone number is (" + ph.DisplayAreaCode() + ") " + ph.DisplayExchange() +"-"+ ph.DisplayNumber());
-
+        System.out.print("My number is ");
+        phone1.display();
+        System.out.print("Your number is ");
+        phone2.display();
     }
 }
